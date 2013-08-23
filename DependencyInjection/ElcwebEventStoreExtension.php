9c773a67d2a0cc4fad02b6db3ea4742c8b53d97d
Etienne Lachance <et@etiennelachance.com>
2013-08-23 12:03:03 -0400

    Add missing use

--- DependencyInjection/ElcwebEventStoreExtension.php
+++ DependencyInjection/ElcwebEventStoreExtension.php
@@ -3,6 +3,7 @@
 namespace Elcweb\EventStoreBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
