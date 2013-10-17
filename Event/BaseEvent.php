a8a6e0386f08f0e600ad799b6162e5b81c2787d4
Etienne Lachance <et@etiennelachance.com>
2013-10-17 18:39:32 -0400

    BaseEvent now extend GenericEvent Make constructor backward compatible

--- Event/BaseEvent.php
+++ Event/BaseEvent.php
@@ -1,15 +1,19 @@
 <?php
 namespace Elcweb\EventStoreBundle\Event;
 
-use Symfony\Component\EventDispatcher\Event;
+use Symfony\Component\EventDispatcher\GenericEvent;
 
-class BaseEvent extends Event
+class BaseEvent extends GenericEvent
 {
     protected $data;
 
-    public function __construct($data)
+    // data2 is for backward compatibility
+    public function __construct($data, $data2 = false)
     {
-        $this->data = $data;
+        $this->data = ($data2) ? $data2 : $data;
+        $subject    = ($data2) ? $data  : null;
+
+        parent::__construct($subject, $this->data);
     }
 
     public function getData()
