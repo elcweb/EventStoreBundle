1ed3eae1c6b3d604cbe65fdaef56045c3fb16266
Etienne Lachance <et@etiennelachance.com>
2014-04-25 09:59:47 -0400

    ELEN-504: Add paginator for index Add jsoneditor and create show view

--- Controller/AdminController.php
+++ Controller/AdminController.php
@@ -2,9 +2,12 @@
 
 namespace Elcweb\EventStoreBundle\Controller;
 
+use Elcweb\EventStoreBundle\Entity\Event;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use JMS\SecurityExtraBundle\Annotation\Secure;
 
 /**
  * @Route("/admin/eventstore")
@@ -14,15 +17,40 @@ class AdminController extends Controller
     /**
      * @Route("/")
      * @Template()
+     * @Secure(roles="ROLE_ADMIN")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        /** @var $em EntityManager */
         $em = $this->getDoctrine()->getManager();
 
+
+        // get the events sorted DESC
         $events = $em->getRepository('ElcwebEventStoreBundle:Event')->findBy(array(), array("createdAt" => "DESC"));
-        
+
+        $paginator  = $this->get('knp_paginator');
+        /** @var $pagination Event[] */
+        $pagination = $paginator->paginate(
+            $events,
+            $this->get('request')->query->get('page', 1) /*page number*/,
+            100/*limit per page*/
+        );
+
         return array(
-            'events' => $events
+            'events' => $pagination,
+
+        );
+    }
+
+    /**
+     * @Route("/show/{event}")
+     * @Template()
+     * @Secure(roles="ROLE_ADMIN")
+     */
+    public function showAction(Event $event)
+    {
+        return array(
+            'event' => $event
         );
     }
 }
